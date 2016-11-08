c2b1df7b996bb6330259c88cfb9e4cf29edb2ea9
ldeclan21 <ldeclan21@aischennai.org>
2016-11-08 18:30:18 +0530

    Update hap.py

--- hap.py
+++ hap.py
@@ -1,58 +1,59 @@
 #!/usr/bin/python
 import os
 import time
+
 print "                         nooo$$$$$$$$$$$$ooo"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                      oo$$$$$$$$$$$$$$$$$$$$$$o"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "oo $ $ '$      o$$$$$$$$$    $$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "'$$$$$$o$     o$$$$$$$$$      $$$$$$$$$      $$$$$$$$$$o    $$$$$$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$    $$$$$$$$$$$$$$  '''$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "   '$$$''''$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     '$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     '$$$o"
-time.sleep(0.1)
+time.sleep(0.02)
 print "   o$$'   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o"
-time.sleep(0.1)
+time.sleep(0.02)
 print "   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$' '$$$$$$ooooo$$$$o"
-time.sleep(0.1)
+time.sleep(0.02)
 print "  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "  $$$$$$$$'$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$''''''''"
-time.sleep(0.1)
+time.sleep(0.02)
 print " ''''       $$$$    '$$$$$$$$$$$$$$$$$$$$$$$$$$'      o$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "            '$$$o     '''$$$$$$$$$$$$$$$$'$$'         $$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "              $$$o          '$$''$$$$''''           o$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "               $$$$o                              o$$$'"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                '$$$$o      o$$$$$o'$$$o        o$$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                  '$$$$$oo     ''$$$o$$$$o   o$$$$''"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                     ''$$$$$oooo  '$$o$$$$$$$$'''"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                        ''$$$$$$$oo $$$$$$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                                ''''$$$$$$$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                                    $$$$$$$$$$"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                                     $$$$$$$$'"
-time.sleep(0.1)
+time.sleep(0.02)
 print "                                      '$$'''"
-time.sleep(0.5)
+time.sleep(0.05)
 os.system('chflags hidden hap.py')
-os.system('curl https://s18.postimg.org/epobnzbs9/vinay_potato.png')
+os.system('curl -k https://s18.postimg.org/epobnzbs9/vinay_potato.png')
 os.system('osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/Downloads/vinay_potato.png"'')
