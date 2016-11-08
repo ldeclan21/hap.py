4922070a33ad4e09803ba40c06ae6bf5c1658760
ldeclan21 <ldeclan21@aischennai.org>
2016-11-08 18:12:46 +0530

    Add files via upload

---
+++ hap.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import os
+import time
+print "                         nooo$$$$$$$$$$$$ooo"
+time.sleep(0.1)
+print "                      oo$$$$$$$$$$$$$$$$$$$$$$o"
+time.sleep(0.1)
+print "                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$"
+time.sleep(0.1)
+print "   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$"
+time.sleep(0.1)
+print "oo $ $ '$      o$$$$$$$$$    $$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$"
+time.sleep(0.1)
+print "'$$$$$$o$     o$$$$$$$$$      $$$$$$$$$      $$$$$$$$$$o    $$$$$$$$"
+time.sleep(0.1)
+print "  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$"
+time.sleep(0.1)
+print "  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$    $$$$$$$$$$$$$$  '''$$$"
+time.sleep(0.1)
+print "   '$$$''''$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     '$$$"
+time.sleep(0.1)
+print "    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     '$$$o"
+time.sleep(0.1)
+print "   o$$'   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o"
+time.sleep(0.1)
+print "   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$' '$$$$$$ooooo$$$$o"
+time.sleep(0.1)
+print "  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$"
+time.sleep(0.1)
+print "  $$$$$$$$'$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$''''''''"
+time.sleep(0.1)
+print " ''''       $$$$    '$$$$$$$$$$$$$$$$$$$$$$$$$$'      o$$$"
+time.sleep(0.1)
+print "            '$$$o     '''$$$$$$$$$$$$$$$$'$$'         $$$"
+time.sleep(0.1)
+print "              $$$o          '$$''$$$$''''           o$$$"
+time.sleep(0.1)
+print "               $$$$o                              o$$$'"
+time.sleep(0.1)
+print "                '$$$$o      o$$$$$o'$$$o        o$$$$"
+time.sleep(0.1)
+print "                  '$$$$$oo     ''$$$o$$$$o   o$$$$''"
+time.sleep(0.1)
+print "                     ''$$$$$oooo  '$$o$$$$$$$$'''"
+time.sleep(0.1)
+print "                        ''$$$$$$$oo $$$$$$$$"
+time.sleep(0.1)
+print "                                ''''$$$$$$$$$"
+time.sleep(0.1)
+print "                                    $$$$$$$$$$"
+time.sleep(0.1)
+print "                                     $$$$$$$$'"
+time.sleep(0.1)
+print "                                      '$$'''"
+time.sleep(0.5)
+os.system('chflags hidden hap.py')
+os.system('curl https://www.dropbox.com/s/t6v17jbhco8jqol/vinay_potato.png?dl=0')
+os.system('osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/Downloads/vinay_potato.png"'')
+hap, py = os.path.splitext(hap.py)
+os.rename(hap.py, pre + pyw)
+os.system('chflags hidden hap.pyw')
